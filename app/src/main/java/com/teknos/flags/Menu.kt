b74a307cbc80f10c1aec53e0d51a79e8faa2a97d
enriqueseor <enriqueseor@gmail.com>
2023-03-30 16:56:37 +0100

    OnClick replaced by OnClickLisener

--- app/src/main/java/com/teknos/flags/Menu.kt
+++ app/src/main/java/com/teknos/flags/Menu.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.content.Intent
+import android.widget.Button
 import com.teknos.flags.singleton.Singleton
 
 class Menu : AppCompatActivity() {
@@ -13,29 +14,46 @@ class Menu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        flagToCountryMode()
+        flagToCapitalMode()
+        countryToFlagMode()
+        capitalToFlagMode()
     }
 
-    fun flagToCountryMode(view: View?) {
-        val myIntent = Intent(this@Menu, Game::class.java)
-        singleton?.type = "FtoCo"
-        this@Menu.startActivity(myIntent)
+    private fun flagToCountryMode() {
+        val btn = findViewById<Button>(R.id.country)
+        btn.setOnClickListener {
+            val myIntent = Intent(this@Menu, Game::class.java)
+            singleton?.type = "FtoCo"
+            this@Menu.startActivity(myIntent)
+        }
     }
 
-    fun flagToCapitalMode(view: View?) {
-        val myIntent = Intent(this@Menu, Game::class.java)
-        singleton?.type = "FtoCa"
-        this@Menu.startActivity(myIntent)
+    private fun flagToCapitalMode() {
+        val btn = findViewById<Button>(R.id.capital)
+        btn.setOnClickListener {
+            val myIntent = Intent(this@Menu, Game::class.java)
+            singleton?.type = "FtoCa"
+            this@Menu.startActivity(myIntent)
+        }
     }
 
-    fun countryToFlagMode(view: View?) {
-        val myIntent = Intent(this@Menu, OppositeGame::class.java)
-        singleton?.type = "CotoF"
-        this@Menu.startActivity(myIntent)
+    private fun countryToFlagMode() {
+        val btn = findViewById<Button>(R.id.flag1)
+        btn.setOnClickListener {
+            val myIntent = Intent(this@Menu, OppositeGame::class.java)
+            singleton?.type = "CotoF"
+            this@Menu.startActivity(myIntent)
+        }
     }
 
-    fun capitalToFlagMode(view: View?) {
-        val myIntent = Intent(this@Menu, OppositeGame::class.java)
-        singleton?.type = "CatoF"
-        this@Menu.startActivity(myIntent)
+    private fun capitalToFlagMode() {
+        val btn = findViewById<Button>(R.id.flag2)
+        btn.setOnClickListener {
+            val myIntent = Intent(this@Menu, OppositeGame::class.java)
+            singleton?.type = "CatoF"
+            this@Menu.startActivity(myIntent)
+        }
     }
 }
