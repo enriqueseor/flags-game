5d66968309719d2a34cd1622a6d74ee071ebb7bc
enriqueseor <enriqueseor@gmail.com>
2023-04-07 13:32:45 +0100

    all names fit into buttons

--- app/src/main/java/com/teknos/flags/database/DatabaseReader.kt
+++ app/src/main/java/com/teknos/flags/database/DatabaseReader.kt
@@ -3,4 +3,4 @@ package com.teknos.flags.database
 import android.content.Context
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper
 
-internal class DatabaseReader(context: Context?) : SQLiteAssetHelper(context, "data.db", null, 1)
+internal class DatabaseReader(context: Context?) : SQLiteAssetHelper(context, "database.db", null, 1)
